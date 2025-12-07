90022e44c19a50e7656a60a5efcb531021591729
bamolala <ayostark7@gmail.com>
2025-12-07 23:07:20 +0100

    Book class feature

---
+++ oop/book_class.py
@@ -0,0 +1,19 @@
+
+class Book:
+    def __init__(self, title, author, year):
+        """Constructor: Initializes a Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor: Called when an instance is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """String representation for end-users."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Official representation to recreate the object."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
